e04fb0d12857f5dca0379331004512003c42cf6d
Alejandro Bores Rivera <boresrivera@gmail.com>
2021-09-16 01:57:51 -0500

    Added first files

---
+++ functions.js
@@ -0,0 +1,28 @@
+Mnr.init({
+  binds:{
+    
+  },
+  loadEnter: function(){
+   if(document.getElementById('pageLoader')){
+      document.getElementById('pageLoader').classList.remove('load');
+   }
+  },
+  loadEnd: function(){
+    if(document.getElementById('pageLoader')){
+      document.querySelector('#pageLoader .glare').classList.remove('expand');
+      document.querySelector('#pageLoader .glare').classList.add('contract');
+      setTimeout(()=>{
+        document.querySelector('#pageLoader .moon').classList.add('load3');
+      },400);
+      setTimeout(()=>{
+        document.getElementById('pageLoader').classList.add('load');
+      },700);
+      setTimeout(()=>{
+        document.querySelector('#pageLoader').classList.add('hide');
+      },1000);
+    }
+  },
+  loadEnterTime: 200,
+  loadEndTime: 500,
+});
+
